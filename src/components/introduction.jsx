7f0e39869cf1ad36a34cb77bd9b34fa5d962cd01
santosh-shrimangale <santoshshrimangale01@gmaiil.com>
2024-05-20 17:31:03 +0530

    Update timeline

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -22,7 +22,7 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -37,14 +37,14 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-<li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
+                          <h1>I often <br />Write ... </h1>
                           <p><a className="btn btn-primary btn-learn" href='#' target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -22,19 +22,51 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>HMX Media<span>2024-present</span></h2>
-                        <p>I have joined the HMX Media as a Software Developer in the Web Devolopment team.</p>
+                        <p>I have joined the HMX Media as a Software Developer in the Web Devolopment team, And Developed, maintained, and optimized real-time web and mobile
+                          experiences for 3D products
+                          Designed custom 3D models tailored to the specific needs of
+                          clients
+                          Worked with global clients including Dell, Lenovo, Royal Enfield,
+                          Sharp, Maclaren, etc.
+                          Utilized technologies such as React JS, TypeScript, WebGL,
+                          WebXR, Web API, and Git
+                          Ensured seamless and immersive user experiences across
+                          platforms</p>
                       </div>
                     </div>
                   </article>
-                  
+
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-4">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Yash<span>2022-2023</span></h2>
-                        <p>I joined the Yash Technologies as a Fresher, UI trainee Developer </p>
+                        <h2>Yash Technologies <span>2022-2023</span></h2>
+                        <p>I joined the Yash Technologies as a Fresher, UI trainee Developer, And Developed production-level code with React, Material UI, Redux
+                          Toolkit, Typescript, and Webpack
+                          Designed and implemented customized, reusable, and
+                          responsive components
+                          Proficient in React Js, Redux, Material UI, and CSS3 for front-end
+                          development
+                          Ensured adherence to best practices for scalable and
+                          maintainable codebases
+                          Collaborated effectively with cross-functional teams to deliver
+                          high-quality solutions on schedule  </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Aavid Software <span>09/2021-02/2022</span></h2>
+                        <p>Completed training in HTML, CSS, JavaScript, Git, and ReactJS, in
+                          front-end development
+                          Contributed to team projects, applying learned skills in building
+                          dynamic user interfaces </p>
                       </div>
                     </div>
                   </article>
